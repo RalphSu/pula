a2d510763d784681ad5f360fdc49c758861a9f70
Su, Ralph <suliangfei@gmail.com>
2015-10-11 23:26:52 +0800

    update sql

--- mssql_db/2015-10-11-update_tc_notice_data.sql
+++ mssql_db/2015-10-11-update_tc_notice_data.sql
@@ -3,9 +3,21 @@ select * from notice
 
 update notice set attachmentKey='Notice:1' where id=1
 update notice set attachmentKey='Notice:2' where id=2
+update notice set attachmentKey='Notice:3' where id=3
+update notice set attachmentKey='Notice:4' where id=4
+update notice set attachmentKey='Notice:5' where id=5
+
 
 select * from time_course
 
 
 update time_course set attachmentKey='TC:1' where id=1
 update time_course set attachmentKey='TC:2' where id=2
+update time_course set attachmentKey='TC:3' where id=3
+update time_course set attachmentKey='TC:4' where id=4
+update time_course set attachmentKey='TC:5' where id=5
+update time_course set attachmentKey='TC:6' where id=6
+update time_course set attachmentKey='TC:7' where id=7
+update time_course set attachmentKey='TC:8' where id=8
+update time_course set attachmentKey='TC:9' where id=9
+
